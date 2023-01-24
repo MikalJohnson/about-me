0d8037df70c60c19cd1940a0b930c0a2fd0ad8f1
Mikal Johnson <Johnsonmikal24@gmail.com>
2023-01-24 15:01:46 -0800

    Started Questions for user

--- js/app.js
+++ js/app.js
@@ -1,1 +1,51 @@
-console.log("hi");
+alert("Lets see how well you know me! O_O")
+
+//Question 1
+let nameCheck = prompt("Is my name spelled Malik? Yes or No?").toLowerCase();
+if (nameCheck === 'no' || nameCheck === 'n') {
+  console.log('You are correct!')
+} else if (nameCheck === 'yes' || nameCheck === 'y') {
+  console.log('Wow! And i thought we were Friends -_-!');
+} else {
+  console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+}
+
+//Question 2
+let servedMil = prompt("Did i serve in the military? Yes or No?").toLowerCase();
+if (servedMil === 'yes' || servedMil === 'y') {
+  console.log('You are correct!')
+} else if (servedMil === 'no' || servedMil === 'n') {
+  console.log('Wow! And i thought we were Friends -_-!');
+} else {
+  console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+}
+
+let medicalField = prompt("Did i ever work in the medical field? Yes or No?").toLowerCase();
+
+if (medicalField === 'No' || medicalField === 'N') {
+  console.log('You are correct!')
+} else if (medicalField === 'no' || medicalField === 'n') {
+  console.log('Wow! And i thought we were Friends -_-!');
+} else {
+  console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+}
+
+let nameCheck = prompt("Is my name spelled Malik? Yes or No?").toLowerCase();
+
+if (nameCheck === 'No' || nameCheck === 'N') {
+  console.log('You are correct!')
+} else if (nameCheck === 'no' || nameCheck === 'n') {
+  console.log('Wow! And i thought we were Friends -_-!');
+} else {
+  console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+}
+
+let nameCheck = prompt("Is my name spelled Malik? Yes or No?").toLowerCase();
+
+if (nameCheck === 'No' || nameCheck === 'N') {
+  console.log('You are correct!')
+} else if (nameCheck === 'no' || nameCheck === 'n') {
+  console.log('Wow! And i thought we were Friends -_-!');
+} else {
+  console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+}
