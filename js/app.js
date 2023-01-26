dbbe62aa106654ca74ab8afac24591ee71430851
Mikal Johnson <Johnsonmikal24@gmail.com>
2023-01-26 09:46:49 -0800

    Added Alerts to questions 1-5

--- js/app.js
+++ js/app.js
@@ -6,20 +6,31 @@
 let nameCheck = prompt("Is my name spelled Malik? Yes or No?").toLowerCase();
 if (nameCheck === 'no' || nameCheck === 'n') {
   console.log('You are correct!')
+  alert('You are correct!')
+
 } else if (nameCheck === 'yes' || nameCheck === 'y') {
   console.log('Wow! And i thought we were Friends -_-!');
+  alert('Wow! And i thought we were Friends -_-!');
+
 } else {
   console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+  alert('you are the one that makes me have to write more code -_-! Yes or No please!')
 }
 
 //Question 2
 let servedMil = prompt("Did i serve in the military? Yes or No?").toLowerCase();
 if (servedMil === 'yes' || servedMil === 'y') {
   console.log('You are correct!')
+  alert('You are correct!')
+
 } else if (servedMil === 'no' || servedMil === 'n') {
   console.log('Wow! And i thought we were Friends -_-!');
+  alert('Wow! And i thought we were Friends -_-!');
+
 } else {
   console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+  alert('you are the one that makes me have to write more code -_-! Yes or No please!')
+
 }
 
 //Question 3
@@ -27,10 +38,16 @@ let medicalField = prompt("Did i ever work in the medical field? Yes or No?").to
 
 if (medicalField === 'yes' || medicalField === 'y') {
   console.log('You are correct!')
+  alert('You are correct!')
+
 } else if (medicalField === 'no' || medicalField === 'n') {
   console.log('Wow! And i thought we were Friends -_-!');
+  alert('Wow! And i thought we were Friends -_-!');
+
 } else {
   console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+ alert('you are the one that makes me have to write more code -_-! Yes or No please!')
+
 }
 
 //Question 4
@@ -38,10 +55,16 @@ let cityCheck = prompt("Was I born in Boston, MA? Yes or No?").toLowerCase();
 
 if (cityCheck === 'no' || cityCheck === 'n') {
   console.log('You are correct! I wish i had the accent though :(')
+  alert('You are correct! I wish i had the accent though :(')
+
 } else if (cityCheck === 'yes' || cityCheck === 'y') {
   console.log('Incorrect, but i wish!');
+  alert('Incorrect, but i wish!');
+
 } else {
   console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+  alert('you are the one that makes me have to write more code -_-! Yes or No please!')
+
 }
 
 //Question 5
@@ -49,10 +72,16 @@ let schoolCheck = prompt("Do i attend class at Coding Dojo? Yes or No?").toLower
 
 if (schoolCheck === 'no' || schoolCheck === 'n') {
   console.log('Damn Right!')
+  alert('Damn Right!')
+
 } else if (schoolCheck === 'yes' || schoolCheck === 'y') {
   console.log('Wow! Maybe you should go to coding dojo -_-! Incorrect');
+  alert('Wow! Maybe you should go to coding dojo -_-! Incorrect');
+
 } else {
   console.log('you are the one that makes me have to write more code -_-! Yes or No please!')
+  alert('you are the one that makes me have to write more code -_-! Yes or No please!')
+
 }
 
 //Question 6
